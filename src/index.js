9d299214e0ca4ef355fe5deb28340dffd6312487
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2022-02-15 23:41:57 +0000

    initialized project

---
+++ src/index.js
@@ -0,0 +1,26 @@
+[
+
+]
+
+// secret = Water
+// guess = Bread
+
+// Water
+// Bread
+
+
+// {a, e, r} -> present-category ()
+// {W, t} -> not present (colour -> black)
+// {a, e, r} -> wrongly placed (colour -> yellow) else (color -> green)
+
+
+//Program to solve wordle
+// guess() -> [pool of 5 lettered strring I have from my dataset]
+
+// score-every-single --> work in my bank and rank them; 
+
+//6 tries
+
+// some letters occur more frequently than others
+// compare letters in the alphabets and rank them based on the probability of occurence
+// 
