d330ab0a5da965dfc139762ffd14ce37710b7aab
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2022-02-17 23:14:54 +0000

    feat: completed basic functionalities of wordle game

--- src/index.js
+++ src/index.js
@@ -19,22 +19,108 @@
 // compare letters in the alphabets and rank them based on the probability of occurence
 //
 import axios from "axios";
+import inquirer from 'inquirer'
+
+const GREEN = ' G '
+const YELLOW = ' Y '
+const BLACK = ' B '
 
 class Wordle {
+    #hints = []
+    #tries = 0
     constructor() {
     this.wordsBank = fetchAllWords().then(res => {
         return res
     })
   }
 
-   get words() {
+    get words() {
       return this.wordsBank
     }
 
-    async guess() {
+    async promptUser() {
+        try {
+            var questions = [
+                {
+                  type: 'input',
+                  name: 'userGuess',
+                  message: "Enter Guess For Today \n"
+                }
+              ]
+            let result = await inquirer.prompt(questions);
+            
+            const found = (await this.words).find(element => element == result.userGuess.toUpperCase());
+            while (!found) {
+                var questions = [
+                    {
+                      type: 'input',
+                      name: 'userGuess',
+                      message: "Word not in WordList \n Enter Another Guess For Today \n"
+                    }
+                  ]
+                  result = await inquirer.prompt(questions);
+            }
+            return result.userGuess.toUpperCase()
+        } catch (err) {
+            throw new Error (err.message)
+        }
+
+    }
+
+    async makeHint(userGuess, secretWord) {
+        try {
+            const hint = []
+            console.log(userGuess, secretWord)
+            for (let i = 0; i < secretWord.length; ++i)
+            {
+                if (userGuess[i] == secretWord[i]) {
+                    hint.push(GREEN)
+                } else if ([...secretWord].includes(userGuess[i])) {
+                    hint.push(YELLOW)
+                } else {
+                    hint.push(BLACK)
+                }
+            }
+            this.#hints = [...this.#hints, hint]
+            return hint
+        } catch (err) {
+            throw new Error(err)
+        }
+    
+    }
+
+    winMessage() {
+        console.log(`\n\n Hey!! You just won today's wordle!! in ` + this.#tries + ` tries \n
+         come back tomorrow for another!!!`)
+    }
+
+    loseMessage() {
+        console.log(`\n\n Sorry (''), you couldn't find the right combination in ` + this.#tries + ` tries \n
+         come back tomorrow for another!!!`)
+    }
+
+    win() {
+        const len = this.#hints.length
+        return this.#hints.length <= 0 ? false : this.#hints[len - 1].every(hint => hint == GREEN)
+    }
+
+    async play() {
+        const secretWord = await this.secretWord()
+        while (this.#tries < 6 && !(this.win())) {
+            const userGuess = await this.promptUser()
+            const hint = await this.makeHint(userGuess, secretWord)
+            this.#tries += 1
+
+            console.log(hint + '\n')
+        } 
+        (this.win()) ? this.winMessage() : this.loseMessage()
+    }
+
+    async secretWord() {
+        let date = new Date();
         let w =  await this.words
-        const rand = getRandomInt(0, w.length);
-        return w[rand]
+        const secreteInd = date.getDay() > w.length ? date.getDay() % w.length : w.length % date.getDay();
+        return w[secreteInd]
     }
 }
 
@@ -42,14 +128,8 @@ const fetchAllWords = async () => {
     const words = await axios.get("http://localhost:3000/words");
     return words.data
 };
-
-const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
  
 export { fetchAllWords, Wordle };
 
 const wordle = new Wordle()
-wordle.guess()
+wordle.play()
